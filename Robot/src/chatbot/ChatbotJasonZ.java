0fef8b162a38a60b6be2076f249a025c4744c948
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-09-29 15:26:02 -0400

    My ChatBot Topic JasonZou

---
+++ Robot/src/chatbot/ChatbotJasonZ.java
@@ -0,0 +1,21 @@
+package chatbot;
+
+public class ChatbotJasonZ implements Topic {
+
+	public ChatbotJasonZ() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void talk(String response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTriggered(String response) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
