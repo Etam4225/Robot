0cdd8df8755ee7a9e74a0e8adccc7c7e51330b5a
Ethan Tam <etam4225@bths.edu>
2017-10-02 15:29:53 -0400

    edited chatbotethan

--- Robot/src/chatbot/ChatbotEthan.java
+++ Robot/src/chatbot/ChatbotEthan.java
@@ -6,22 +6,45 @@ public class ChatbotEthan implements Topic {
 	private String goodbyeKeyword;
 	private String secretKeyword;
 	private String response;
+	private String YES = "YES";
+	private String NO = "NO";
+	private boolean saidYesorNo;
 	
 	public ChatbotEthan() {
-		String[] temp = {"movie","lord of the rings"};
+		String[] temp = {"movie","lord of the rings", "watching"};
 		
 		keywords = temp;
 		goodbyeKeyword = "bye";
-		secretKeyword = "pug";
+		secretKeyword = "Lord of the Rings";
+		
 		response = "";
 	}
 
 	public void talk(String response) {
-		ChatbotMain.print("Hey! So you want to talk about generic boring things huh? I love talking about that. So tell me something");
+		ChatbotMain.print("Movies eh? I love movies to be honest. Hey, " + ChatbotMain.chatbot.getUsername() + ", you like movies man? Yes or no?");
 		response = ChatbotMain.getInput();
+		saidYesorNo = false;
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
+			if(ChatbotMain.findKeyword(response, YES, 0) >= 0) {
+				
+			}
+			else
+			{
+				if(ChatbotMain.findKeyword(response, NO, 0) >= 0) {
+					
+				}
+				else {
+					while(!saidYesorNo) {
+						ChatbotMain.print("Uhhh...just say yes or no plz");
+						response = ChatbotMain.getInput();			
+						if(ChatbotMain.findKeyword(response, YES, 0) >= 0 || ChatbotMain.findKeyword(response, NO, 0) >= 0) {
+							saidYesorNo = true;
+						}
+					}
+				}
+			}
 			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
-				ChatbotMain.print("I can't even. I love pugs so much. Wow. You are so cool.");
+				ChatbotMain.print("OH MAN ITS THE BEST MAN");
 				response = ChatbotMain.getInput();
 			}else {
 				ChatbotMain.print("Yeah. That's pretty cool. But there are things I like even more. Tell me something else.");
@@ -42,5 +65,9 @@ public class ChatbotEthan implements Topic {
 		}
 		return false;
 	}
+	public String startGame() {
+		return "xd";
+	}
+	
 
 }
