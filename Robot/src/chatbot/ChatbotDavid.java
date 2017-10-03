8bdd08c32a5ce54e1d7332bdde7333d1b5741684
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-10-03 14:32:07 -0400

    Updated

--- Robot/src/chatbot/ChatbotDavid.java
+++ Robot/src/chatbot/ChatbotDavid.java
@@ -6,24 +6,31 @@ public class ChatbotDavid implements Topic {
 	private String goodbyeKeyword;
 	private String secretKeyword;
 	private String response;
+	private String[] hints;
+	private String confirmation;
 	
 	public ChatbotDavid() {
-		String[] temp = {"stuff", "things"};
+		String[] temp = {""};
 		keywords = temp;
 		goodbyeKeyword = "bye";
-		secretKeyword = "pug";
+		secretKeyword = "Harry Potter";
 		response = "";
+		String[] hintList = {""};
+		hints = hintList;
+		//String[] confirmationGame = {"Sure", "Ok", "Why not", "Yes"};
+		confirmation = "Ok";
 	}
 
 	public void talk(String response) {
-		ChatbotMain.print("Hey! So you like books? I know pretty much everything about books! So tell me a book you like!");
+		ChatbotMain.print("Hey! So you like reading books? So do I! I have a game in mind, let's play!");
 		response = ChatbotMain.getInput();
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
-			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
-				ChatbotMain.print("I can't even. I love pugs so much. Wow. You are so cool.");
+			if(ChatbotMain.findKeyword(response, confirmation, 0) >= 0) {
+				ChatbotMain.print("Great! Here's how to play: You have 5 guesses to think of the book I am thinking of right now. "
+						+ "Don't worry I'll give you hints. Here's your first one: ");
 				response = ChatbotMain.getInput();
 			}else {
-				ChatbotMain.print("Yeah. That's pretty cool. But there are things I like even more. Tell me something else.");
+				ChatbotMain.print("That's no fun. It's be an easy game! Let's play!");
 				response = ChatbotMain.getInput();
 			}
 		}
