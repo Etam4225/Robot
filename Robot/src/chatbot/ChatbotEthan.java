f88f8c3d74c736a5be9d6a55966efae4e2db065d
Ethan Tam <etam4225@bths.edu>
2017-10-03 14:54:57 -0400

    chatbotEthan update

--- Robot/src/chatbot/ChatbotEthan.java
+++ Robot/src/chatbot/ChatbotEthan.java
@@ -17,7 +17,7 @@ public class ChatbotEthan implements Topic {
 		String[] temp = {"movie", "Lord of the Rings", "watch"};
 		keywords = temp;
 		goodbyeKeyword = "bye";
-		String[] botAnnoyed = {"for real tho pls", "seriously. stop", "ok this is your last chance to say yes or no."};
+		String[] botAnnoyed = {"Just say yes or no >:(","for real tho pls", "seriously. stop", "ok this is your last chance to say yes or no."};
 		annoyed = botAnnoyed;
 		secretKeyword = "N/A";
 		response = "";
@@ -34,15 +34,24 @@ public class ChatbotEthan implements Topic {
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
 			while(!saidYesorNo) {
 				if(ChatbotMain.findKeyword(response, YES, 0) >= 0) {
+					printMessage("Good to know "+ ChatbotMain.chatbot.getUsername() + " !");
+				}
+				while(annoyedCounter < 4) {
+					//or is it less than 3?
+					printMessage(annoyed[annoyedCounter]);
+					annoyedCounter++;
+				}
+				if(annoyedCounter == 3) {
+					printMessage("alright im done with you, " +ChatbotMain.chatbot.getUsername()+". Bye.");
+					ChatbotMain.chatbot.startChatting();
+				}
 					
-				}	
-				printMessage(annoyed[annoyedCounter]);
-				annoyedCounter++;
 				
 			}
 			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
 				ChatbotMain.print("I can't even. I love pugs so much. Wow. You are so cool.");
 				response = ChatbotMain.getInput();
+				//remove?
 			}else {
 				ChatbotMain.print("Yeah. That's pretty cool. But there are things I like even more. Tell me something else.");
 				response = ChatbotMain.getInput();
