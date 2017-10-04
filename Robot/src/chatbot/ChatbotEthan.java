9136e967145748f90ccca8fe750200a73942c513
Ethan Tam <etam4225@bths.edu>
2017-10-04 14:47:00 -0400

    update chatbotEthan

--- Robot/src/chatbot/ChatbotEthan.java
+++ Robot/src/chatbot/ChatbotEthan.java
@@ -6,31 +6,50 @@ public class ChatbotEthan implements Topic {
 	private String goodbyeKeyword;
 	private String secretKeyword;
 	private String response;
+	
 	private String[] annoyed;
 	private boolean saidYesorNo;
 	private int annoyedCounter;
+	
 	private String previousInput;
 	private int convoCount;
-	private boolean sameInput;
+
+	private String[] movieArray;
+	private String chosenMovie;
 	
+	private String[] movieQuestionArray;
+	
+	private double myRandNum;
+	
+	//CONSTANTS
 	private String YES;
 	private String NO;
+	private String START;
 	
 	public ChatbotEthan() {
-		String[] temp = {"movie", "Lord of the Rings", "watch"};
+		String[] temp = {"movie"};
 		keywords = temp;
 		goodbyeKeyword = "bye";
+		
 		String[] botAnnoyed = {"Just say yes or no >:(", "...", "for real tho pls", "seriously. stop", "ok this is your last chance to say yes or no."};
 		annoyed = botAnnoyed;
-		secretKeyword = "N/A";
-		response = "";
 		saidYesorNo = false;
 		annoyedCounter = 0;
-		convoCount = 0;
-		sameInput = false;
 		
+		secretKeyword = "N/A";
+		response = "";
+		convoCount = 0; //dupe responses doesnt work yet
+		
+		String[] myMovieArray = {"Lord of the Rings"};
+		movieArray = myMovieArray;
+		chosenMovie = "";
+		
+		myRandNum = 0;
+		
+		//CONSTANTS
 		YES = "yes";
 		NO = "no";
+		START = "start";
 	}
 
 	public void talk(String response) {
@@ -38,31 +57,50 @@ public class ChatbotEthan implements Topic {
 		response = ChatbotMain.getInput();
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
 			while(!saidYesorNo) {
-				previousInput = response;
+				/*previousInput = response;
 				if(previousInput.equals(response) && convoCount >= 1) {
 					printMessage("Ever thought of saying something unique?");
 					sameInput = true;
-				}
+				}*/
 				if(ChatbotMain.findKeyword(response, YES, 0) >= 0 && response.length() == 3) {
-					printMessage("Good to know "+ ChatbotMain.chatbot.getUsername() + "!");
+					printMessage("Good to know "+ ChatbotMain.chatbot.getUsername() + "! So do you want to play a game called Guess the Movie? Say start and we can start!");
 					saidYesorNo = true;
-				}
-				if(ChatbotMain.findKeyword(response, NO, 0) >= 0 && response.length() == 2) {
-					printMessage("Ah shucks "+ ChatbotMain.chatbot.getUsername() + "...still wanna play a game anyways?");
-					saidYesorNo = true;
-				}
-				else {
-					printMessage(annoyed[annoyedCounter]);
 					response = ChatbotMain.getInput();
-					if(annoyedCounter == 4) {
-						printMessage("alright im done with you, " +ChatbotMain.chatbot.getUsername()+". Bye.");
-						ChatbotMain.chatbot.startChatting();
+					if(ChatbotMain.findKeyword(response, START, 0) >= 0) {
+						initiateGame();
+						//start the guessing game
 					}
 					else {
-						annoyedCounter++;
+						
+						//exit and ask diff question etc.?
 					}
 				}
-				sameInput = false;
+				if(ChatbotMain.findKeyword(response, NO, 0) >= 0 && response.length() == 2) {
+					printMessage("Ah shucks "+ ChatbotMain.chatbot.getUsername() + "...still wanna play a game anyways? If you say start ill start a game!");
+					saidYesorNo = true;
+					response = ChatbotMain.getInput();
+					if(ChatbotMain.findKeyword(response, START, 0) >= 0) {
+						initiateGame();
+						//start guessing game
+					}
+					else {
+		
+					}
+				}
+				else {
+					if(!saidYesorNo) {
+						printMessage(annoyed[annoyedCounter]);
+						response = ChatbotMain.getInput();
+						if(annoyedCounter == 4) {
+							printMessage("alright im done with you, " +ChatbotMain.chatbot.getUsername()+". Bye.");
+							annoyedCounter = 0;
+							ChatbotMain.chatbot.startChatting();
+						}
+						else {
+							annoyedCounter++;
+						}
+					}
+				}
 				convoCount++;
 				
 			}
@@ -81,6 +119,16 @@ public class ChatbotEthan implements Topic {
 	}
 	public void printMessage(String message) {
 		ChatbotMain.print(message);
+	}
+	public double createRandNum() {
+		myRandNum = Math.random();
+		return myRandNum;
+	}
+	public void initiateGame() {
+		if(myRandNum < 1) {
+			chosenMovie = movieArray[0];
+		}
+		printMessage(chosenMovie + " is the movie that I want you to guess");
 	}
 	public boolean isTriggered(String response) {
 		for(int i = 0; i < keywords.length; i++) {
